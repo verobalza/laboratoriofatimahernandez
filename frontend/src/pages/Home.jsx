3219511305e108af67456301f496f67db576ec05
verobalza <veronicabalza19@gmail.com>
2026-03-05 09:32:32 +0100

    Forzar deploy del frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
+    
     <div className="home-container">
-      <h1>Frontend funcionando ✨</h1>
+
+      <h1>Frontend funcionando --version 2 ✨</h1>
       <p>Bienvenido al proyecto fullstack Laboratorio</p>
       <div className="info-box">
         <h2>Información del Proyecto</h2>
